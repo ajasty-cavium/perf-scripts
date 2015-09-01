2cacceced655a284765aef768ceabc2a7a121421
Joel Jones <jjones@cavium.com>
2015-09-01 20:38:07 +0000

    Fix len bug, allow setting bin paths, programmatically get # of cores.

--- perf-30.sh
+++ perf-30.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+# Allow easy changing of which perf, read_perf, and enable_EL0 are used
+#PERFBIN=../sdk.build/linux-aarch64/tools/perf/perf
+PERFBIN=./perf
+#READBIN=../read_counter/read_perf/read_perf
+READBIN=./read_perf
+#ENABLEBIN=../read_counter/enable_EL0/enable_EL0
+ENABLEBIN=./enable_EL0
 
 len=$1
 cntval=1
-ncores=96
+ncores=`nproc`
 freq=2000000000
 
-if [ "xlen" == "x" ]
+if [ "x$len" == "x" ]
 then
     len=30
 fi
@@ -16,15 +23,15 @@ function getcnt {
 
 #./getfg.sh
 
-./perf stat -a -er11 -- sleep 1 2> /tmp/perf1.out
+$PERFBIN stat -a -er11 -- sleep 1 2> /tmp/perf1.out
 getcnt r11
 r11=$cntval
-freq=$(($r11/$ncores))
+freq=$(( $r11 / $ncores ))
 #msfreq=$(($freq/1000))
 msfreq=$freq
 echo "freq=$freq, msfreq=$msfreq"
 
-./perf stat -a -er16,r17,r3,r42,r40,re6 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er16,r17,r3,r42,r40,re6 -- sleep $len 2> /tmp/perf1.out
 
 getcnt r16
 r16=$(($cntval))
@@ -49,7 +56,7 @@ echo "Avg cycles/dmiss = $((($re6*100000)/$r17))"
 echo "CDMISS latency = $((($re6 + ($r3*8))/$r3))"
 echo "Time spent in CDMISS = $(($re6/(($msfreq)*$ncores)))ms"
 
-./perf stat -a -er1f9,r1fa,r1fb,r193,rdf,r1f2 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er1f9,r1fa,r1fb,r193,rdf,r1f2 -- sleep $len 2> /tmp/perf1.out
 
 getcnt r1f9
 r1f9=$(($cntval*100000))
@@ -73,7 +80,7 @@ echo "DMB instructions issued = $r1f2"
 echo "STR stalls on NOWBUFS = $rdf"
 echo "Cycles spent in PTW = $r193 / $(($r193/(($msfreq)*$ncores)))ms"
 
-./perf stat -a -erce,rcc,rc8,r90,r68,r2d -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -erce,rcc,rc8,r90,r68,r2d -- sleep $len 2> /tmp/perf1.out
 
 getcnt rce
 rce=$cntval
@@ -89,7 +96,7 @@ getcnt r2d
 r2d=$cntval
 echo "rce=$rce rcc=$rcc rc8=$rc8 r90=$r90 r68=$r68 r2d=$r2d"
 
-./perf stat -a -er23,r24,ref,r1a,r10,rd8 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er23,r24,ref,r1a,r10,rd8 -- sleep $len 2> /tmp/perf1.out
 
 getcnt r23
 r23=$cntval
@@ -110,7 +117,7 @@ echo "icache stall time = $(($rd8/($msfreq*$ncores)))ms"
 echo "throttle stall time = $(($ref/($msfreq*$ncores)))ms"
 echo "Unaligned replay penalty = $((($rcc*8)/($msfreq*$ncores)))ms"
 
-./perf stat -a -ercf,re7,r187,re8,rc7,r7a -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -ercf,re7,r187,re8,rc7,r7a -- sleep $len 2> /tmp/perf1.out
 
 getcnt rcf
 rcf=$cntval
@@ -127,7 +134,7 @@ r7a=$cntval
 
 echo "rcf=$rcf re7=$re7 r187=$r187 re8=$re8 rc7=$rc7 r7a=$r7a"
 
-./perf stat -a -er1b,rf,r10,r70,rde,r71 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er1b,rf,r10,r70,rde,r71 -- sleep $len 2> /tmp/perf1.out
 
 getcnt r1b
 r1b=$cntval
@@ -143,7 +150,7 @@ getcnt r71
 r71=$cntval
 
 echo "r1b=$r1b rf=$rf r10=$r10 r70=$r70 rde=$rde r71=$r71"
-./perf stat -a -erc2,rc3,rc4,rc1,rc0,r11 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -erc2,rc3,rc4,rc1,rc0,r11 -- sleep $len 2> /tmp/perf1.out
 
 getcnt rc2
 rc2=$cntval
@@ -170,7 +177,7 @@ echo "2 issue cycles = $(($rc4/$iclk))"
 echo "Single-issued instruction% = $(($rc3/$issp))"
 echo "Double-issued instruction% = $((($rc4/$issp)*2))"
 
-./perf stat -a -er5,rde,r8,r6,r7,rc -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er5,rde,r8,r6,r7,rc -- sleep $len 2> /tmp/perf1.out
 
 getcnt r5
 r5=$cntval
@@ -187,7 +194,7 @@ rc=$cntval
 
 echo "r5=$r5 rde=$rde r3=$r3 re6=$re6 r8=$r8 r6=$r6 r7=$r7 rc=$rc"
 
-./perf stat -a -er1b,rb,re,r21,r22,r25 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er1b,rb,re,r21,r22,r25 -- sleep $len 2> /tmp/perf1.out
 getcnt r1b
 r1b=$cntval
 getcnt rb
@@ -203,7 +210,7 @@ r25=$cntval
 
 echo "r1b=$r1b rb=$rb re=$re r21=$r21 r22=$r22 r25=$r25 ipc=$((($r8*100)/$rc0))"
 
-./perf stat -a -ercb,rcd,rce,rcf,r90,r91 -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -ercb,rcd,rce,rcf,r90,r91 -- sleep $len 2> /tmp/perf1.out
 getcnt rcb
 rcb=$cntval
 getcnt rcd
@@ -219,7 +226,7 @@ r91=$cntval
 
 echo "rcb=$rcb rcd=$rcd rce=$rce rcf=$rcf r90=$r90 r91=$r91"
 
-./perf stat -a -er78,r79,r7a,r40,r41,rc -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er78,r79,r7a,r40,r41,rc -- sleep $len 2> /tmp/perf1.out
 getcnt r78
 r78=$cntval
 getcnt r79
@@ -235,7 +242,7 @@ rc=$cntval
 
 echo "r78=$r78 r79=$r79 r7a=$r7a r40=$r40 r41=$r41 rc=$rc"
 
-./perf stat -a -er3,r4,r9,ra,rd,rf -- sleep $len 2> /tmp/perf1.out
+$PERFBIN stat -a -er3,r4,r9,ra,rd,rf -- sleep $len 2> /tmp/perf1.out
 getcnt r3
 r3=$cntval
 getcnt r4
@@ -251,16 +258,18 @@ rf=$cntval
 
 echo "rc3=$r3 r4=$r4 r9=$r9 ra=$ra rd=$rd rf=$rf"
 
-./enable_EL0
+$ENABLEBIN
 
+#sudo ../read_counter/read_perf/read_perf -s 0 -e 4 -f a.txt -i 3 -r 0x4e02 --count 2
+echo len: $len
 echo "4d00,4d02,4d04,4d05,4d06,4d08"
-./read_perf -s 0 -e 0 -r 4d00 -r 4d02 -r 4d04 -r 4d05 -r 4d06 -r 4d08 --count $len -i 1
+$READBIN -s 0 -e 4 -f - -r 0x4d00 -r 0x4d02 -r 0x4d04 -r 0x4d05 -r 0x4d06 -r 0x4d08 --count $len -i 1
 
 echo "4d0a,4d0c,4d15,4d1e,4d20,4d40"
-./read_perf -s 0 -e 0 -r 4d0a -r 4d0c -r 4d15 -r 4d1e -r 4d20 -r 4d40 --count $len -i 1
+$READBIN -s 0 -e 1 -r -f - 4d0a -r 4d0c -r 4d15 -r 4d1e -r 4d20 -r 4d40 --count $len -i 1
 
 echo "4da5,4da7,4da9,4d30,4d32,4d42"
-./read_perf -s 0 -e 0 -r 4da5 -r 4da7 -r 4da9 -r 4d30 -r 4d32 -r 4d42 --count $len -i 1
+$READBIN -s 0 -e 1 -r -f - 4da5 -r 4da7 -r 4da9 -r 4d30 -r 4d32 -r 4d42 --count $len -i 1
 
 echo "\n\n"
 
