5678e5d5d932722562629f6a766254865a4b39e5
Ananth Jasty <ajasty@cavium.com>
2016-10-28 18:49:16 -0700

    Committed getcm-sort.sh, printing lines where time is spent.

---
+++ getcm-sort.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+perf record -F 99 $@
+perf script | sed -e 's/(//' -e 's/)//' | awk '{ printf("%s\t%s\t%s\t%s\n", $4, $8, $6, $7); }' | sort -nr > a2l.txt
+
+cat a2l.txt | while read p; 
+do
+    ADDR=`echo $p | awk '{ print $3; }'`
+    PROG=`echo $p | awk '{ print $2; }'`
+    #echo $p | awk '{ print $3; }'
+    #echo "hi $ADDR $PROG"
+    if [ -e $PROG ]
+    then
+	addr2line -e $PROG $ADDR
+    fi
+done 
+
